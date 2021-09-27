7c4dcea8235dfdef796122e7754c61087cecd9f8
Roosevelt Diaz <rdt-2012@hotmail.com>
2021-09-27 00:05:18 -0500

    first commit

---
+++ assets/preguntas.js
@@ -0,0 +1,40 @@
+export default {
+  data:  [{
+      "pregunta": "¿Cómo puedo fortalecer mi intestino y microbiota?",
+      "respuestas": [{
+        "respuesta": "Teniendo hábitos saludables de alimentación",
+        "correcta": false
+      },
+      {
+        "respuesta": "Haciendo ejercicio regularmente",
+        "correcta": false
+      },
+      {
+        "respuesta": "Consumiendo Probióticos",
+        "correcta": false
+      },
+      {
+        "respuesta": "Todas las anteriores",
+        "correcta": true
+      }]
+    },
+    {
+      "pregunta": "¿Qué cepa probiótica tiene Bionutre Inmufort?",
+      "respuestas": [{
+        "respuesta": "Bacillus clausii",
+        "correcta": false
+      },
+      {
+        "respuesta": "S. boulardii",
+        "correcta": false
+      },
+      {
+        "respuesta": "Bacillus coagulans",
+        "correcta": true
+      },
+      {
+        "respuesta": "Lasctobacillus acidophilus",
+        "correcta": false
+      }]
+    }]
+}
